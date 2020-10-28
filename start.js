44589580e7e72a84141949d4a916a7551548623d
Olga Pavlenko <olgaigpavlenko@gmail.com>
2020-10-28 23:01:12 +0200

    color-switch

---
+++ start.js
@@ -0,0 +1,35 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+
+  const body = document.querySelector('body');
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  
+  const startButton = document.querySelector('button[data-action=start]');
+  const stopButton = document.querySelector('button[data-action=stop]');
+
+  let intervalId = null;
+  let i = true;
+
+  startButton.addEventListener('click', onClickChangeBodyColor);
+  stopButton.addEventListener('click', onClickStopChangeBodyColor);
+ 
+  function onClickChangeBodyColor () {
+    if (i === true) {
+      intervalId = setInterval(() => {let color = colors[randomIntegerFromInterval(0, 5)];
+        body.style.backgroundColor = color}, 1000);
+        i = false;
+    } 
+  }
+
+function onClickStopChangeBodyColor () {
+    clearInterval(intervalId);
+}
