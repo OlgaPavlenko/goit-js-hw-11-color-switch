502550a4706ce0fbbb976addbb7b8e54fc3cc43c
Olga Pavlenko <olgaigpavlenko@gmail.com>
2020-11-13 16:08:15 +0200

    resolved issue

--- start.js
+++ start.js
@@ -1,34 +1,51 @@
 const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-  ];
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
 
-  const body = document.querySelector('body');
-  const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+const body = document.querySelector('body');
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-  
-  const startButton = document.querySelector('button[data-action=start]');
-  const stopButton = document.querySelector('button[data-action=stop]');
+const startButton = document.querySelector('button[data-action=start]');
+const stopButton = document.querySelector('button[data-action=stop]');
 
-  let intervalId = null;
-  let i = true;
+function allowStart() {
+  stopButton.disabled = true;
+  startButton.disabled = false;
+}
 
-  startButton.addEventListener('click', onClickChangeBodyColor);
-  stopButton.addEventListener('click', onClickStopChangeBodyColor);
- 
-function onClickChangeBodyColor() {
-      i = true;
-      intervalId = setInterval(() => {let color = colors[randomIntegerFromInterval(0, colors.length)];
-      body.style.backgroundColor = color}, 1000);
-      i = false;
-  }
+function denyStart() {
+  stopButton.disabled = false;
+  startButton.disabled = true;
+}
 
-function onClickStopChangeBodyColor () {
-    clearInterval(intervalId);
+let intervalId = null;
+let i = true;
+
+allowStart();
+
+startButton.addEventListener('click', onStartClick);
+stopButton.addEventListener('click', onStopClick);
+
+function onStartClick() {
+  i = true;
+
+  intervalId = setInterval(() => {
+    let color = colors[randomIntegerFromInterval(0, colors.length)];
+    body.style.backgroundColor = color;
+  }, 1000);
+
+  i = false;
+  denyStart();
+}
+
+function onStopClick() {
+  clearInterval(intervalId);
+  allowStart();
 }
